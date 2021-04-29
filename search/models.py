a7a7971c9911fe2c9bdab9dba4cd757537a0f9c9
Seojun Youn <92sursumcorda@gmail.com>
2021-04-29 16:21:07 +0900

    Amenity table legalcode를 foreignkey로 변경

--- search/models.py
+++ search/models.py
@@ -3,11 +3,10 @@ from django.db import models
 class Amenity(models.Model):
     name         = models.CharField(max_length = 20)
     road_address = models.CharField(max_length = 20)
-    legalcode    = models.CharField(max_length = 20)
+    legalcode    = models.ForeignKey("homes.DongType", on_delete = models.SET_NULL, null = True)
     latitude     = models.DecimalField(max_digits = 15, decimal_places = 10) # 위도
     longitude    = models.DecimalField(max_digits = 15, decimal_places = 10) # 경도
     type         = models.ForeignKey("AmenityType", on_delete = models.SET_NULL, null = True)
-
     class Meta:
         db_table = "amenities"
         
