a2d56ce976cc9b9d659aed1c421384e8d4d418b7
Seojun Youn <92sursumcorda@gmail.com>
2021-04-29 16:21:07 +0900

    Amenity table에 legalcode 추가

---
+++ search/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Amenity(models.Model):
+    name         = models.CharField(max_length = 20)
+    road_address = models.CharField(max_length = 20)
+    legalcode    = models.CharField(max_length = 20)
+    latitude     = models.DecimalField(max_digits = 15, decimal_places = 10) # 위도
+    longitude    = models.DecimalField(max_digits = 15, decimal_places = 10) # 경도
+    type         = models.ForeignKey("AmenityType", on_delete = models.SET_NULL, null = True)
+    class Meta:
+        db_table = "amenities"
+        
+class AmenityType(models.Model):
+    name = models.CharField(max_length = 20)
+    
+    class Meta:
+        db_table = "amenity_types"
