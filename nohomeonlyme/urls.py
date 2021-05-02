7455b6ab6f461e27e09ef784f3535d3696423536
Seojun Youn <92sursumcorda@gmail.com>
2021-05-02 13:40:04 +0900

    map filtering view 작성  - review 내용 반영  - code convension 적용  - error message 삭제  - 필터 data에 min/max 적용  - constant 처리

--- nohomeonlyme/urls.py
+++ nohomeonlyme/urls.py
@@ -2,4 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("room", include("homes.urls")),
+    path('search', include("search.urls")),
+    path('user', include("users.urls"))
 ]
